c1f689b6328b6026c4b0ca70d27f0bc48329f138
syakadev <syakadev@gmail.com>
2025-09-30 13:01:50 +0700

    membuat file mcr untuk semua untity

---
+++ asramaku/app/Http/Controllers/DutyScheduleController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DutySchedule;
+use Illuminate\Http\Request;
+
+class DutyScheduleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(DutySchedule $dutySchedule)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(DutySchedule $dutySchedule)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, DutySchedule $dutySchedule)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(DutySchedule $dutySchedule)
+    {
+        //
+    }
+}
---
+++ asramaku/app/Models/Attendance.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Attendance extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     * @var array<int, string>
+     */
+    protected $fillable = [];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     * @var array<int, string>
+     */
+    protected $hidden = [];
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | RELATIONS
+    |--------------------------------------------------------------------------
+    */
+
+    // /**
+    //  * Get the parent that owns the Attendance
+    //  *
+    //  * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+    //  */
+    // public function parent(): BelongsTo
+    // {
+    //     return $this->belongsTo(Parent::class, 'foreign_key', 'owner_key');
+    // }
+
+    // /**
+    //  * Get all of the children for the Attendance
+    //  *
+    //  * @return \Illuminate\Database\Eloquent\Relations\HasMany
+    //  */
+    // public function children(): HasMany
+    // {
+    //     return $this->hasMany(Child::class, 'foreign_key', 'local_key');
+    // }
+}

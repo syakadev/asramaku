048dba0fda3af9467bbb140a48da896d5b89c26c
syakadev <syakadev@gmail.com>
2025-10-08 10:14:16 +0700

    menambakan route infraction

--- routes/web.php
+++ routes/web.php
@@ -8,10 +8,12 @@ Route::get('/', function () {
 
 use App\Http\Controllers\DormFundController;
 use App\Http\Controllers\PerformController;
+use App\Http\Controllers\InfractionController;
 use App\Http\Controllers\ActivityController;
 use App\Http\Controllers\AttendanceController;
 
 Route::resource('dormfunds', DormFundController::class);
+Route::resource('infraction', InfractionController::class);
 Route::resource('performs', PerformController::class);
 Route::resource('activities', ActivityController::class);
 Route::resource('attendances', AttendanceController::class);

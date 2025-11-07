da34185d7e5e659e9c23d8bd89aae5309e3f4a58
syakadev <syakadev@gmail.com>
2025-11-07 22:31:50 +0700

    menambahkan column date kedalam dormfund

--- database/migrations/2025_09_26_043259_create_infractions_table.php
+++ database/migrations/2025_09_26_043259_create_infractions_table.php
@@ -13,8 +13,9 @@ return new class extends Migration
     {
         Schema::create('infractions', function (Blueprint $table) {
             $table->id();
-             $table->string('img');
+            $table->string('img');
             $table->text('note')->nullable();
+            $table->date('date');
             $table->enum('type', ['piket', 'kerapian dan kebersihan']);
             $table->enum('status', ['dibayar', 'belum dibayar'])->default('belum dibayar');
             $table->decimal('amount', 15, 2)->default(0);

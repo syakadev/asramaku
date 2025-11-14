f23c503b0ec345d1a37ec88ad2936fc61179c52f
syakadev <syakadev@gmail.com>
2025-11-15 05:42:49 +0700

    merubah migration atribut period menjadi start date dan end date

--- database/migrations/2025_09_26_043305_create_duty_schedules_table.php
+++ database/migrations/2025_09_26_043305_create_duty_schedules_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
             $table->id();
             $table->foreignId('duty_id')->constrained()->onDelete('cascade');
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->date('period');
+            $table->date('start_date');
+            $table->date('end_date');
             $table->timestamps();
         });
     }

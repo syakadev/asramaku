d2f56828b55f3254f670246f832ca8e8c1bbb036
syakadev <syakadev@gmail.com>
2025-11-24 11:15:00 +0700

    memperbaiki  route index

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return redirect()->route('dormfunds.index');
+})->name('home');
+
 
 use App\Http\Controllers\DormFundController;
 use App\Http\Controllers\PerformController;

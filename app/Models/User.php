44a0207402e4464837fd4dcb5ada8c530c90b5cc
syakadev <syakadev@gmail.com>
2025-11-24 10:52:40 +0700

    memberikan relasi dutyschedule

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,4 +47,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function dutySchedules()
+    {
+        return $this->hasMany(DutySchedule::class);
+    }
 }

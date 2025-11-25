0a25a7249a47b7f1f6539e02ebfbf75c183ecc3e
syakadev <syakadev@gmail.com>
2025-11-25 14:49:55 +0700

    menambahkan mcr untuk dokumentasi kegiatan ( documentationActivities

---
+++ app/Http/Controllers/DocumentationActivitiesController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\documentationActivities;
+use Illuminate\Http\Request;
+
+class DocumentationActivitiesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(documentationActivities $documentationActivities)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(documentationActivities $documentationActivities)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, documentationActivities $documentationActivities)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(documentationActivities $documentationActivities)
+    {
+        //
+    }
+}

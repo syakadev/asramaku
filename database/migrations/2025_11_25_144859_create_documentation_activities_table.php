01ab84ecb5e31ec8bcbd45250845efca0d788c67
syakadev <syakadev@gmail.com>
2025-11-25 15:03:37 +0700

    foreign key dan imagae

--- database/migrations/2025_11_25_144859_create_documentation_activities_table.php
+++ database/migrations/2025_11_25_144859_create_documentation_activities_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('documentation_activities', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('activities_id')->constrained()->onDelete('cascade');
+            $table->string('img');
             $table->timestamps();
         });
     }

a22d64f905621acdeb0627b0b627a397b65026e8
syakadev <syakadev@gmail.com>
2025-11-25 21:08:08 +0700

    menambahkan route documentation

--- routes/web.php
+++ routes/web.php
@@ -11,17 +11,19 @@ use App\Http\Controllers\DormFundController;
 use App\Http\Controllers\PerformController;
 use App\Http\Controllers\InfractionController;
 use App\Http\Controllers\ActivityController;
+use App\Http\Controllers\DocumentationActivitiesController;
 use App\Http\Controllers\AttendanceController;
 use App\Http\Controllers\DutyController;
 use App\Http\Controllers\DutyScheduleController;
 use App\Http\Controllers\LostItemController;
 
-
 Route::resource('dormfunds', DormFundController::class);
 Route::resource('infractions', InfractionController::class);
 Route::resource('performs', PerformController::class);
 Route::resource('activities', ActivityController::class);
+Route::resource('documentations', DocumentationActivitiesController::class);
 Route::resource('attendances', AttendanceController::class);
 Route::resource('duties', DutyController::class);
 Route::resource('dutySchedules', DutyScheduleController::class);
 Route::resource('lostitems', LostItemController::class);
+
